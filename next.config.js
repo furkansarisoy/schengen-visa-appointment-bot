5219372d72ccf27a504505c4bc969ae7c2db3990
fsarisoy <furkan.sarisoy@dk.migros.com.tr>
2025-03-17 17:59:39 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/schengen-visa-appointment-bot' : '',
+  output: "export",
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/schengen-visa-appointment-bot/' : '',
   trailingSlash: true,
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
